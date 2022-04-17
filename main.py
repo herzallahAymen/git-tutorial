dd9ff961c09226de33280f52538cee77e56b9b10
herzallahAymen <66031645+herzallahAymen@users.noreply.github.com>
2022-04-17 20:53:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ name = input("give me a name")
 #hey there
 print("hello world! i am "+name)
 
+while True:
+  n = int(input('give me a number between 0 and 5'))
+  if n == 3 :
+    print('right')
+  else:
+    print('wrong')
+    
+print('you have won the game')
